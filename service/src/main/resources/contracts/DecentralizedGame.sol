d407d0114c41c8efcaf577755e3b365c845b87ab
Julius Lauterbach <julius.lauterbach@nttdata.com>
2025-05-19 23:42:38 +0200

    code style: refactor DecentralizedGame contract by removing unused console logs and consolidating getter functions

--- service/src/main/resources/contracts/DecentralizedGame.sol
+++ service/src/main/resources/contracts/DecentralizedGame.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.15;
 
-//import "hardhat/console.sol";
-
 contract DecentralizedGame {
 
     event GameCreated(address manager, string message);
@@ -90,19 +88,10 @@ contract DecentralizedGame {
 
     constructor(address _manager, uint256 _verificationFeedbackBlocks) {
         gameState = GameState.Created;
-        //console.log("Game contract deployed by:", tx.origin);
         manager = _manager;
         verificationFeedbackBlocks = _verificationFeedbackBlocks;
         emit GameCreated(manager, "some instructions");
         gameState = GameState.Betting;
-    }
-
-    /**
-     * @dev Return manager address
-     * @return address of manager
-     */
-    function getManager() external view returns (address) {
-        return manager;
     }
 
     function bet(bytes32 _numberHash) external payable bettingPhase {
@@ -136,7 +125,6 @@ contract DecentralizedGame {
         for (uint i = 0; i < bets.length; i++) {
             if (bets[i].player == msg.sender) {
                 require(uint256(hash) == uint256(bets[i].hashedNumber), "chosenNumber or secretPassword input incorrect");
-                //console.log("chosenNumber '%s' verified by: '%s'", _chosenNumber, msg.sender);
                 bets[i].verifiedChosenNumber = _chosenNumber;
                 bets[i].verified = true;
             }
@@ -153,14 +141,6 @@ contract DecentralizedGame {
 
     //maybe not needed for this implementation, maybe directly to endGame
     function beginEvaluation(string memory message) external isPlayer verificationPhase {
-        //not necessary cause verifyEncryptedNumber would do this step automatically
-        /*bool allBetsVerified = true;
-        for (uint i = 0; i < bets.length; i++) {
-            if (bets[i].verified == false) {
-                allBetsVerified = false;
-            }
-        }
-        require(verificationUntilBlock < block.number || allBetsVerified, "evaluation cant start because the verificationUntilBlock is not reached or not all bets are verified");*/
         require(verificationUntilBlock < block.number, "evaluation cant start because the verificationUntilBlock is not reached");
 
         gameState = GameState.Evaluation;
@@ -172,33 +152,62 @@ contract DecentralizedGame {
 
         managerFee = (getBalance() * 10) / 100;
         winnerPrize = (getBalance() * 90) / 100;
-        //console.log("number of winners: %s, so the winnerPrize is: %s", winner, winnerPrize);
 
-        //console.log("transferring winnerPrize '%s' to '%s'", winnerPrize, winner);
         winner.transfer(winnerPrize);
         emit WinnerAnnouncement(winner, winnerPrize, winnerBet);
 
-        //console.log("transferring managerFee '%s' to manager '%s'", managerFee, manager);
         payable(manager).transfer(managerFee);
-
         gameState = GameState.Ended;
     }
 
-    function determineWinner() internal evaluationPhase returns (address payable, uint16) {
-        //console.log("determineWinner - by players average number");
+    /**
+     * @dev Return manager address
+     * @return address of manager
+     */
+    function getManager() external view returns (address) {
+        return manager;
+    }
 
+    function getPossibleWinnerList() external view gameEnded returns (address payable[] memory) {
+        return possibleWinnerList;
+    }
+
+    function getWinner() external view gameEnded returns (address payable) {
+        return winner;
+    }
+
+    function getWinnerPrize() external view gameEnded returns (uint) {
+        return winnerPrize;
+    }
+
+    function getManagerFee() external view gameEnded returns (uint) {
+        return managerFee;
+    }
+
+    function getBets() external view returns (Bet[] memory) {
+        return bets;
+    }
+
+    function getGameState() external view returns (GameState) {
+        return gameState;
+    }
+
+    function getVerificationFeedbackBlocks() external view returns (uint256) {
+        return verificationFeedbackBlocks;
+    }
+
+    function getBalance() public view returns (uint){
+        return address(this).balance;
+    }
+
+    function determineWinner() internal evaluationPhase returns (address payable, uint16) {
         uint sum = 0;
         for (uint i = 0; i < bets.length; i++) {
             if (bets[i].verified) {
                 sum += bets[i].verifiedChosenNumber;
-            } else {
-                //console.log("bet from %s is not verified, so its not part of the sum", bets[i].player);
             }
         }
-        //console.log("sum of all (verified) players equals: %s", sum);
-
         uint average = (sum / bets.length) * 2 / 3;
-        //console.log("two third average is: %s", average);
 
         uint lowestDiff = 1001;
 
@@ -208,22 +217,16 @@ contract DecentralizedGame {
                 if (diff < 0) {
                     diff *= - 1;
                 }
-                //console.log("next checked bet is: '%s' from '%s', with a diff of '%s'", bets[i].verifiedChosenNumber, bets[i].player, uint(diff));
                 if (lowestDiff > uint(diff)) {
                     delete possibleWinnerList;
                     delete winnerBetList;
-                    //console.log("old lowestDiff is: %s, new one is: %s, emptied winner and winnerBet arrays", lowestDiff, uint(diff));
                     lowestDiff = uint(diff);
                     possibleWinnerList.push(bets[i].player);
                     winnerBetList.push(bets[i].verifiedChosenNumber);
                 } else if (lowestDiff == uint(diff)) { //if diff is same, push
-                    //console.log("same lowestDiff: %s as the current winner, push the new winner additionally to the winner array: '%s'", uint(diff), bets[i].player);
-
                     possibleWinnerList.push(bets[i].player);
                     winnerBetList.push(bets[i].verifiedChosenNumber);
                 }
-            } else {
-                //console.log("bet from %s is not verified", bets[i].player);
             }
         }
         if (possibleWinnerList.length > 1) {
@@ -233,40 +236,14 @@ contract DecentralizedGame {
     }
 
     function getRandomWinner(address payable[] memory _winnerList, uint16[] memory _winnerBetList) internal view returns (address payable, uint16) {
-        uint256 hash = uint256(keccak256(abi.encodePacked(_winnerList, _winnerBetList, block.number, block.timestamp, blockhash(block.number - 1))));
+        uint256 hash = uint256(keccak256(abi.encodePacked(
+            _winnerList,
+            _winnerBetList,
+            block.number,
+            block.timestamp,
+            blockhash(block.number - 1)
+        )));
         uint256 m = hash % _winnerList.length;
         return (_winnerList[m], _winnerBetList[m]);
-    }
-
-    function getBalance() public view returns (uint){
-        return address(this).balance;
-    }
-
-    function getPossibleWinnerList() public view gameEnded returns (address payable[] memory){
-        return possibleWinnerList;
-    }
-
-    function getWinner() public view gameEnded returns (address payable){
-        return winner;
-    }
-
-    function getWinnerPrize() public view gameEnded returns (uint){
-        return winnerPrize;
-    }
-
-    function getManagerFee() public view gameEnded returns (uint){
-        return managerFee;
-    }
-
-    function getBets() public view returns (Bet[] memory){
-        return bets;
-    }
-
-    function getGameState() public view returns (GameState){
-        return gameState;
-    }
-
-    function getVerificationFeedbackBlocks() public view returns (uint256){
-        return verificationFeedbackBlocks;
     }
 }
