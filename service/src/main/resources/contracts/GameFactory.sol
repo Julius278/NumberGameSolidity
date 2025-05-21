a8b8566f4fe0f18feeebb2407fb49864318a65bf
Julius Lauterbach <git@juliuslauterbach.de>
2025-05-21 21:19:55 +0200

    code style

--- service/src/main/resources/contracts/GameFactory.sol
+++ service/src/main/resources/contracts/GameFactory.sol
@@ -1,13 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.15;
 
-//import "hardhat/console.sol";
-
-import {ManagedGame} from "./ManagedGame.sol";
-import {DecentralizedGame} from "./DecentralizedGame.sol";
+import "./ManagedGame.sol";
+import "./DecentralizedGame.sol";
 
 contract GameFactory {
-
     ManagedGame[] internal mGames;
     DecentralizedGame[] internal dGames;
 
@@ -30,14 +27,10 @@ contract GameFactory {
     }
 
     function getLastManagedGameAddress() public view returns (address) {
-        //console.log("number of managed games ", mGames.length);
-        //console.log("address of first managed games ", address(mGames[0]));
         return address(mGames[0]);
     }
 
     function getLastDecentralizedGameAddress() public view returns (address) {
-        //console.log("number of decentralized games ", dGames.length);
-        //console.log("address of first decentralized games ", address(dGames[0]));
         return address(dGames[0]);
     }
 }
