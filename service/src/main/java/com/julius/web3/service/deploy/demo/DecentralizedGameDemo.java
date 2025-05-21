afad5092c267f5a6de891f4817b1003a8758e4f1
Julius Lauterbach <git@juliuslauterbach.de>
2025-05-21 21:19:49 +0200

    update DecentralizedGameDemo to improve gas fee calculation and switch to StaticEIP1559GasProvider

--- service/src/main/java/com/julius/web3/service/deploy/demo/DecentralizedGameDemo.java
+++ service/src/main/java/com/julius/web3/service/deploy/demo/DecentralizedGameDemo.java
@@ -10,7 +10,11 @@ import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.gas.ContractGasProvider;
+import org.web3j.tx.gas.DefaultGasProvider;
 import org.web3j.tx.gas.DynamicEIP1559GasProvider;
+import org.web3j.tx.gas.StaticEIP1559GasProvider;
+
+import java.math.BigInteger;
 
 
 public class DecentralizedGameDemo {
@@ -25,10 +29,19 @@ public class DecentralizedGameDemo {
 
 		// setup for general node query
 		final Web3j web3jConnection = Web3j.build(new HttpService(EVM_NODE_ADDRESS));
-		long chainID = Long.parseLong(web3jConnection.netVersion().send().getNetVersion());
+		//long chainID = Long.parseLong(web3jConnection.netVersion().send().getNetVersion());
+		long chainID = web3jConnection.ethChainId().send().getChainId().longValue();
 		LOGGER.info("connected node: {}", web3jConnection.web3ClientVersion().send().getWeb3ClientVersion());
 		LOGGER.info("current block number: {}", web3jConnection.ethBlockNumber().send().getBlockNumber());
 		LOGGER.info("chainID: {}", chainID);
+
+		BigInteger maxPriorityFeePerGas = web3jConnection.ethMaxPriorityFeePerGas().send().getMaxPriorityFeePerGas();
+		BigInteger baseFee =
+				web3jConnection.ethGetBlockByNumber(DefaultBlockParameterName.LATEST, false)
+						.send()
+						.getBlock()
+						.getBaseFeePerGas();
+		BigInteger maxFeePerGas = baseFee.multiply(BigInteger.valueOf(2)).add(maxPriorityFeePerGas);
 
 		// setup for sending transactions
 		final Credentials credentials = WalletUtils.loadCredentials(KEY_FILE_PASSWORD, KEY_FILE_PATH);
@@ -37,10 +50,11 @@ public class DecentralizedGameDemo {
 		//System.out.println(web3jConnection.ethGasPrice().send().getGasPrice());
 
 
-		final ContractGasProvider gasProvider = new DynamicEIP1559GasProvider(web3jConnection, chainID);
-		//final ContractGasProvider gasProvider = new DefaultGasProvider();
+		//final ContractGasProvider gasProvider = EVM_NODE_ADDRESS.contains("localhost") ? new DefaultGasProvider() : new DynamicEIP1559GasProvider(web3jConnection, chainID);
+		//final ContractGasProvider gasProvider = new DynamicEIP1559GasProvider(web3jConnection, chainID);
+		final ContractGasProvider gasProvider = new StaticEIP1559GasProvider(chainID, maxFeePerGas, maxPriorityFeePerGas, BigInteger.valueOf(9000000L));
 
-
+		LOGGER.info("deploying GameFactory");
 		GameFactory factory = GameFactory.deploy(web3jConnection, credentials, gasProvider).send();
 
 		LOGGER.info("deployed GameFactory with address {}", factory.getContractAddress());
